69750bcdb795e4456327541c9733ad8044ccb0a4
mouadhx04 <mouadh1500@gmail.com>
2021-07-30 13:47:26 -0700

    the header file

---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,26 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+/**
+ * struct print - print type and print function
+ * @char_to_compare: print type
+ * @f: print function
+ */
+typedef struct number_complex
+{
+	double re;
+	double im;
+} complex;
+
+void display_complex_number(complex c);
+complex conjugate(complex c);
+double modulus(complex c);
+double argument(complex c);
+void addition(complex c1, complex c2, complex *c3);
+void substraction(complex c1, complex c2, complex *c3);
+void multiplication(complex c1, complex c2, complex *c3);
+void division(complex c1, complex c2, complex *c3);
+void complex_from_mod_arg (double m, double arg, complex *c3);
+
+#endif
